32701c3669441155ea819c53eb0ae74bb524c3a4
tayeful912 <kazitayefulislam12@gmail.com>
2022-02-13 02:28:24 +0600

    sub total ,tax ,total function added

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// common function
+// // common function
+let subTotal =0;
+// // Quantity
 
-// Quantity
 
 function updateProductQuantity(product, price, isIncresing) {
-
 
 
     let productQuantity = document.getElementById(product + '-total');
@@ -13,36 +13,88 @@ function updateProductQuantity(product, price, isIncresing) {
 
     if (isIncresing) {
         productQuantity.value = totalProduct + 1;
+    
     }
     else if (productQuantity.value > 0) {
+
         productQuantity.value = totalProduct - 1;
+        
+
+     
     }
     totalProduct = productQuantity.value;
+
+   
 
 
     //Update quantity total 
 
-    let getProductPrice = document.getElementById(product + '-price')
+    let getProductPrice = document.getElementById(product + '-price');
     getProductPrice.innerText = totalProduct * price;
+
+calculateTotal()
+
+}
+
+function getInputValue(product){
+
+
+    let inputValue = document.getElementById(product +'-total').value;
+    return inputValue;
 
 }
 
 
-//Update quantity total 
+function calculateTotal(){
+
+    let totalPhone = getInputValue('phone') * 1219;
+    let totalCase = getInputValue('case') * 59
+     let subTotal = totalPhone + totalCase;
+
+     let tax = subTotal /10;
+
+     let total = subTotal + tax;
+     console.log(total);
+
+     document.getElementById('subtotal').innerText = subTotal;
+     document.getElementById('tax').innerText = tax;
+     document.getElementById('total').innerText =total;
+
+}
 
 
+
+// phone total
+
+// click button : increasing product quantity
+document.getElementById('phone-plus').addEventListener('click',function(){
+
+    updateProductQuantity('phone',1219,true);
+
+})
+
+// click button: decreesing product quantity
+
+document.getElementById('phone-minus').addEventListener('click',function(){
+
+    updateProductQuantity('phone',1219,false);
+})
+
+
+
+
+
+//Update case total 
 
 document.getElementById('case-plus').addEventListener('click', function () {
 
-    updateProductQuantity('case', 59, true)
-
+    updateProductQuantity('case', 59, true);
 
 })
 
 document.getElementById('case-minus').addEventListener('click', function () {
 
     updateProductQuantity('case', 59, false);
-
 
 })
 
